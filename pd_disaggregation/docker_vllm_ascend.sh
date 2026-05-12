10461651e257c380b8013914a0f9a8257b2b83d2
yyz3172 <yangyinzuo3172@outlook.com>
2026-05-12 14:59:26 +0800

    增加docker_vllm_ascend.sh

---
+++ pd_disaggregation/docker_vllm_ascend.sh
@@ -0,0 +1,29 @@
+# Update --device according to your device (Atlas A2: /dev/davinci[0-7] Atlas A3:/dev/davinci[0-15]).
+# Update the vllm-ascend image according to your environment.
+# Note you should download the weight to /root/.cache in advance.
+
+# 通过命令行第一个参数传入版本号，默认值为 v0.13.0rc1
+# 用法: ./script.sh [version]
+# 示例: ./script.sh v0.14.0
+VERSION=${1:-v0.13.0rc1}
+export IMAGE=quay.io/ascend/vllm-ascend:${VERSION}
+
+docker run --rm \
+    --name vllm-ascend-yyz-${VERSION} \
+    --shm-size=10g \
+    --net=host \
+    --privileged \
+    --device /dev/davinci_manager \
+    --device /dev/devmm_svm \
+    --device /dev/hisi_hdc \
+    -v /usr/local/dcmi:/usr/local/dcmi \
+    -v /usr/local/Ascend/driver/tools/hccn_tool:/usr/local/Ascend/driver/tools/hccn_tool \
+    -v /usr/local/bin/npu-smi:/usr/local/bin/npu-smi \
+    -v /usr/local/Ascend/driver/lib64/:/usr/local/Ascend/driver/lib64/ \
+    -v /usr/local/Ascend/driver/version.info:/usr/local/Ascend/driver/version.info \
+    -v /etc/ascend_install.info:/etc/ascend_install.info \
+    -v /mnt/paas/kubernetes/kubelet/huawei/yyz/.cache:/root/.cache \
+    -v /mnt/paas/kubernetes/kubelet/huawei/yyz:/root/yyz \
+    -v /mnt/paas/kubernetes/kubelet/huawei/xrx:/root/xrx \
+    -v /root/autodl-tmp:/root/autodl-tmp \
+    -it $IMAGE bash
